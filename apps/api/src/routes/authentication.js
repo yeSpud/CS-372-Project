aecf565e5c46b3f4bf64a6043bf3f27a4605279c
Spud <sogden3@alaska.edu>
2024-01-29 15:40:56 -0900

    Added example of how to use http errors

--- apps/api/src/routes/authentication.js
+++ apps/api/src/routes/authentication.js
@@ -1,8 +1,18 @@
+const { BadRequest } = require("http-errors")
 const { Login } = require("./schema/authentication");
 
 const routes = async function(fastify) {
 
     fastify.post("/login", { schema: { Login } }, async request => {
+
+        if (request.body.username === undefined) {
+            throw new BadRequest("Username cannot be undefined")
+        }
+
+        if (request.body.password === undefined) {
+            throw new BadRequest("Password cannot be undefined")
+        }
+
         return `Username: ${request.body.username}, Password: ${request.body.password}`
     })
 
