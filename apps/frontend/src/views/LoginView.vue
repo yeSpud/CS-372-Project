3108e36034da3035062e368d2bc8698981fbb143
Spud <sogden3@alaska.edu>
2024-01-31 17:00:41 -0900

    Got different submit buttons to call different functions

--- apps/frontend/src/views/LoginView.vue
+++ apps/frontend/src/views/LoginView.vue
@@ -1,24 +1,52 @@
 <script setup>
+import { ref } from "vue"
+import { reset } from "@formkit/vue"
+
+const myForm = ref(null)
+async function login() {
+    console.log("Login clicked!")
+    console.log(myForm.value.node.value)
+    // TODO Send request to server
+    reset(myForm.value.node)
+}
+
+async function signup() {
+    console.log("Signup clicked!")
+    console.log(myForm.value.node.value)
+    // TODO Send request to server
+    reset(myForm.value.node)
+}
 </script>
 
 <template>
     <main>
         <h2>Login</h2>
         <FormKit
-            type="text"
-            label="UID"
-            validation="required|min:4|alpha|lowercase|"
-            validation-visibility="live" />
-
-        <FormKit
-            type="password"
-            label="pwd"
-            validation="required|min:8|contains_uppercase|contains_lowercase|contains_numeric|contains_symbol"
-            validation-visibility="live" />
-
-        <button>Login</button>
-      <button>Signup</button>
+            type="form"
+            ref=myForm
+            :actions="false">
+            <FormKit
+                type="text"
+                name="username"
+                label="UID"
+                validation="required|min:4|alpha|lowercase|"
+                validation-visibility="live" />
+            <FormKit
+                type="password"
+                name="password"
+                label="pwd"
+                validation="required|min:8|contains_uppercase|contains_lowercase|contains_numeric|contains_symbol"
+                validation-visibility="live" />
+            <div>
+                <FormKit type="button" label="Login" @click="login" />
+                <FormKit type="button" label="Signup" @click="signup" />
+            </div>
+        </FormKit>
     </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+body {
+    background-color: #b7410e;
+}
+</style>
