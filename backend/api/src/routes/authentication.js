24f7a7fa070eddb96696be715a6eb6513b7eb9a2
Spud <sogden3@alaska.edu>
2024-02-12 21:15:58 -0900

    Added database functions

--- backend/api/src/routes/authentication.js
+++ backend/api/src/routes/authentication.js
@@ -10,24 +10,25 @@ const routes = async function(fastify) {
             throw new NotFound("That username does not exist. Please create an account.")
         }
 
-        await database.updateInvalidLoginAttempts(request.body.username)
+        await database.removeOldLoginAttempts(request.body.username)
 
         const attempts = await database.getInvalidLoginAttempts(request.body.username)
-
-        if (attempts >= 5) {
+        if (attempts.length >= 5) {
             throw new TooManyRequests("Account is locked due to too many failed login attempts.")
         }
 
+        // TODO Make sure password matches regex!
+
         if (!await database.passwordMatches(request.body.username, request.body.password)) {
             await database.addInvalidLoginAttempt(request.body.username)
-            throw new Unauthorized(`Incorrect password. ${5-(attempts+1)} attempts remaining.`)
+            throw new Unauthorized(`Incorrect password. ${5-(attempts.length+1)} attempts remaining.`)
         }
 
-        const session = await database.setSession(request.body.username)
+        const session = await database.setSession(request.body.username, request.cookies["sessionId"])
 
         //return `Username: ${request.body.username}, Password: ${request.body.password}`
         //return { user: "", session: ""}
-        return { session: session } // TODO return the session ID here as well as a cookie
+        return { session: session }
     })
 
 }
--- backend/database/index.js
+++ backend/database/index.js
@@ -5,4 +5,103 @@ const defaultData = { users: [] }
 
 const db = new Low(new JSONFile("db.json"), defaultData)
 
-export default db
+async function addUserToDatabase(username, password) {
+
+    await db.read()
+    if (db.data === null) {
+        throw new Error("Database not initialized correctly and is in a corrupted state!")
+    }
+
+    if (db.data.users.find(user => user.username === username) !== undefined) {
+        throw new Error("User already exists in database")
+    }
+
+    await db.update(data => {
+        data.users.push({
+            username: username,
+            password: password,
+            session: null,
+            loginAttempts: []
+        })
+    })
+}
+
+async function userInDatabase(username) {
+    await db.read()
+
+    if (db.data === null) {
+        return false
+    }
+
+    return db.data.users.find(user => user.username === username) !== undefined
+}
+
+async function passwordMatches(username, password) {
+
+    await db.read()
+
+    if (db.data === null) {
+        return false
+    }
+
+    const user = db.data.users.find(u => u.username === username)
+    return user.password === password
+}
+
+async function updateInvalidLoginAttempts(username) {
+
+    await db.update(data => {
+        data.users.map(user => {
+            if (user.username === username) {
+                const updatedLoginAttempts = []
+                const currentDate = new Date().getTime()
+                user.loginAttempts.forEach(loginAttempt => {
+                    // 24 hours in a day
+                    // 60 minutes in an hour
+                    // 60 seconds in a minute
+                    // 1000 ms in a second
+                    if (currentDate -  loginAttempt.getTime() < (24 * 60 * 60 * 60 * 1000)) {
+                        updatedLoginAttempts.push(loginAttempt)
+                    }
+                })
+                user.loginAttempts = updatedLoginAttempts
+            }
+        })
+    })
+}
+
+async function getInvalidLoginAttempts(username) {
+    await db.read()
+
+    if (db.data === null) {
+        return []
+    }
+
+    const user = db.data.users.find(u => u.username === username)
+    return user.loginAttempts
+}
+
+async function addInvalidLoginAttempt(username) {
+    await db.update(data => {
+        data.users.map(user => {
+            if (user.username === username) {
+                user.loginAttempts.push(new Date())
+            }
+        })
+    })
+}
+
+async function setSessionCookie(username, sessionCookie) {
+    await db.update(data => {
+        data.users.map(user => {
+            if (user.username === username) {
+                user.session = sessionCookie
+            }
+        })
+    })
+
+    return sessionCookie
+}
+
+module.exports = { createNewUser: addUserToDatabase, userInDatabase, passwordMatches, removeOldLoginAttempts: updateInvalidLoginAttempts,
+    getInvalidLoginAttempts, addInvalidLoginAttempt, setSession: setSessionCookie }
