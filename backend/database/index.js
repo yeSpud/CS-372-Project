d47de54b8288e67c3eec0b24f81566714ea1c36c
Spud <sogden3@alaska.edu>
2024-02-12 21:55:37 -0900

    Use builtin node file reading and writing instead of library

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,104 +1,127 @@
-import { Low } from 'lowdb'
-import { JSONFile } from 'lowdb/node'
+const fs = require("node:fs/promises")
 
-const defaultData = { users: [] }
+const filePath = "db.json"
 
-const db = new Low(new JSONFile("db.json"), defaultData)
+async function readFromDatabase() {
+    const data = await fs.readFile(filePath, "utf-8")
+    return JSON.parse(data) // Todo verify the json schema
+}
 
 async function addUserToDatabase(username, password) {
 
-    await db.read()
-    if (db.data === null) {
+    let data = await readFromDatabase()
+    if (data === null || data.users === undefined) {
         throw new Error("Database not initialized correctly and is in a corrupted state!")
     }
 
-    if (db.data.users.find(user => user.username === username) !== undefined) {
+    if (data.users.find(user => user.username === username) !== undefined) {
         throw new Error("User already exists in database")
     }
 
-    await db.update(data => {
-        data.users.push({
-            username: username,
-            password: password,
-            session: null,
-            loginAttempts: []
-        })
+    data.users.push({
+        username: username,
+        password: password,
+        session: null,
+        loginAttempts: []
     })
+
+    await fs.writeFile(filePath, data)
 }
 
 async function userInDatabase(username) {
-    await db.read()
-
-    if (db.data === null) {
+    const data = await readFromDatabase()
+    if (data === null || data.users === undefined) {
         return false
     }
 
-    return db.data.users.find(user => user.username === username) !== undefined
+    return data.users.find(user => user.username === username) !== undefined
 }
 
 async function passwordMatches(username, password) {
 
-    await db.read()
-
-    if (db.data === null) {
+    const data = await readFromDatabase()
+    if (data === null || data.users === undefined) {
         return false
     }
 
-    const user = db.data.users.find(u => u.username === username)
+    const user = data.users.find(u => u.username === username)
     return user.password === password
 }
 
 async function updateInvalidLoginAttempts(username) {
 
-    await db.update(data => {
-        data.users.map(user => {
-            if (user.username === username) {
-                const updatedLoginAttempts = []
-                const currentDate = new Date().getTime()
-                user.loginAttempts.forEach(loginAttempt => {
-                    // 24 hours in a day
-                    // 60 minutes in an hour
-                    // 60 seconds in a minute
-                    // 1000 ms in a second
-                    if (currentDate -  loginAttempt.getTime() < (24 * 60 * 60 * 60 * 1000)) {
-                        updatedLoginAttempts.push(loginAttempt)
-                    }
-                })
-                user.loginAttempts = updatedLoginAttempts
-            }
-        })
+    let data = readFromDatabase()
+    if (data === null || data.users === undefined) {
+        return
+    }
+
+    const user = data.users.find(u => u.username === username)
+    if (user === undefined) {
+        return
+    }
+
+    const updatedLoginAttempts = []
+    const currentDate = new Date().getTime()
+    user.loginAttempts.forEach(loginAttempt => {
+        // 24 hours in a day
+        // 60 minutes in an hour
+        // 60 seconds in a minute
+        // 1000 ms in a second
+        if (currentDate -  loginAttempt.getTime() < (24 * 60 * 60 * 60 * 1000)) {
+            updatedLoginAttempts.push(loginAttempt)
+        }
     })
+    user.loginAttempts = updatedLoginAttempts
+
+    // Todo check if the user is indeed updated...
+    await fs.writeFile(filePath, data)
 }
 
 async function getInvalidLoginAttempts(username) {
-    await db.read()
-
-    if (db.data === null) {
+    const data = readFromDatabase()
+    if (data === null || data.users === undefined) {
         return []
     }
 
-    const user = db.data.users.find(u => u.username === username)
+    const user = data.users.find(u => u.username === username)
     return user.loginAttempts
 }
 
 async function addInvalidLoginAttempt(username) {
-    await db.update(data => {
-        data.users.map(user => {
-            if (user.username === username) {
-                user.loginAttempts.push(new Date())
-            }
-        })
-    })
+
+    let data = await readFromDatabase()
+    if (data === null || data.users === undefined) {
+        return
+    }
+
+    const user = data.users.find(u => u.username === username)
+    if (user === undefined) {
+        return
+    }
+
+    user.loginAttempts.push(new Date())
+
+    // Todo check if the user is indeed updated...
+    await fs.writeFile(filePath, data)
 }
 
 async function setSessionCookie(username, sessionCookie) {
-    await db.update(data => {
-        data.users.map(user => {
-            if (user.username === username) {
-                user.session = sessionCookie
-            }
-        })
-    })
+
+    let data = await readFromDatabase()
+    if (data === null || data.users === undefined) {
+        return
+    }
+
+    const user = data.users.find(u => u.username === username)
+    if (user === undefined) {
+        return
+    }
+
+    user.session = sessionCookie
+
+
+    // Todo check if the user is indeed updated...
+    await fs.writeFile(filePath, data)
 
     return sessionCookie
 }
