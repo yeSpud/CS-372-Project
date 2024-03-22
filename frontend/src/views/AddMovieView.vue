1bc0e76c4e852029678c02e1946990fe410a418f
Alaskanyoda <112357290+Alaskanyoda@users.noreply.github.com>
2024-03-21 18:59:19 -0800

    Update AddMovieView.vue
    make code pretty

--- frontend/src/views/AddMovieView.vue
+++ frontend/src/views/AddMovieView.vue
@@ -21,7 +21,7 @@ async function addMovie() {
   })
 
   try {
-    const response = await fetch('http://localhost:8080/movies', {
+    const response = await fetch("http://localhost:8080/movies", {
       credentials: "include",
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -44,63 +44,63 @@ async function addMovie() {
   }
 }
 
-const genreList = ['Action', 'Scifi', 'Thriller', 'Romance', 'Drama']
+const genreList = ["Action", "Scifi", "Thriller", "Romance", "Drama"]
 </script>
 
 <template>
   <main class="container">
     <h2 align="center" style="color: white">Add A Movie</h2>
     <div v-if="error !== ''" class="alert alert-danger" role="alert">Error: {{ error }}</div>
-    <div v-if="submitSuccess" class="alert alert-success" role="alert">Successfully added movie!</div>
+    <div v-if="submitSuccess" class="alert alert-success" role="alert">
+      Successfully added movie!
+    </div>
     <FormKit class="form-group" type="form" ref="myForm" @submit="addMovie" :actions="false">
-      
       <FormKit
         type="text"
         name="name"
         label="Movie Name"
         input-class="form-control"
-        :validation="[
-          ['required']
-        ]"
+        :validation="[['required']]"
         :validation-messages="{
           required: 'A movie name is required',
           characterMax: 'Movie name must be under 200 characters long'
         }"
         validation-visibility="live"
       />
-      
-      <FormKit 
+
+      <FormKit
         type="select"
         name="genre"
         label="Genre(s)"
         placeholder=" "
-        input-class="form-control" 
+        input-class="form-control"
         :options="genreList"
-        :validation="[
-          ['required']
-        ]"
+        :validation="[['required']]"
         :validation-messages="{
           required: 'A movie name is required',
           characterMax: 'Movie name must be under 200 characters long'
         }"
-        />
-      
+      />
+
       <FormKit
         type="text"
         name="movieUrl"
         label="Movie URL"
         input-class="form-control"
-        :validation="[
-          ['required']
-        ]"
+        :validation="[['required']]"
         validation-visibility="live"
         :validation-messages="{
           required: 'A movie URL is required'
         }"
       />
-      
+
       <div class="row btn-group btn-group-lg" role="group">
-        <FormKit input-class="btn" type="button" label="Cancle" @click="router.push({ name: 'home' })"/>
+        <FormKit
+          input-class="btn"
+          type="button"
+          label="Cancle"
+          @click="router.push({ name: 'home' })"
+        />
         <FormKit input-class="btn" type="submit" label="Submit" />
       </div>
     </FormKit>
